071e9625ee10f053fe4d5a5b6bce64ef8f60b95f
dipanshusolanki <deepanshusolanki@gmail.com>
2024-07-11 13:04:08 +0530

    Counter App Created

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount((prev) => prev - 1);
+        }}
+      >
+        Decrement
+      </button>
+    </>
+  );
+}
+
+export default App;
